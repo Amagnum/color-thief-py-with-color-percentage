9efb4d005d067cbf22d3aaed316adb780deb3113
Adarsh Baghel <43698929+Amagnum@users.noreply.github.com>
2023-08-12 14:34:55 +0530

    Update colorthief.py

--- colorthief.py
+++ colorthief.py
@@ -354,8 +354,9 @@ class CMap(object):
 
     @property
     def palette(self):
-        return self.vboxes.map(lambda x: x['color'])
-
+        total = sum(self.vboxes.map(lambda x: x['vbox'].count))
+        return self.vboxes.map(lambda x: x['color'] + (x['vbox'].count, total, int(x['vbox'].count / float(total) * 100)))
+    
     def push(self, vbox):
         self.vboxes.push({
             'vbox': vbox,
